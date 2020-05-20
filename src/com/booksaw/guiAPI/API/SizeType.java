e6c478dc5c9241fc0019ee14d08442b69e82c157
James McNair <booksaw224@gmail.com>
2020-05-20 12:30:18 +0100

    Improved formatting

--- src/com/booksaw/guiAPI/API/SizeType.java
+++ src/com/booksaw/guiAPI/API/SizeType.java
@@ -2,21 +2,41 @@ package com.booksaw.guiAPI.API;
 
 /**
  * Used to determine how the size is calculated
+ * 
  * @author booksaw
  *
  */
 public enum SizeType {
-SMALLEST/** Used to make the gui as small as possible*/, LARGEST/** makes the gui size = Gui.MAXSIZE*/, BLANKROW /** Same as smallest but leaves a blank row at the bottom*/,CUSTOM/** Allows the dev to specify a custom size for the gui (Gui.size)*/;
+	/**
+	 * Used to make the gui as small as possible
+	 */
+	SMALLEST,
+
+	/**
+	 * makes the gui size = Gui.MAXSIZE
+	 */
+	LARGEST,
+
+	/**
+	 * Same as smallest but leaves a blank row at the bottom
+	 */
+	BLANKROW,
+
+	/**
+	 * Allows the dev to specify a custom size for the gui (Gui.size)
+	 */
+	CUSTOM;
 
 	/**
 	 * Uses the last item to calculate the size of the inventory
+	 * 
 	 * @param lastItem - the location of the final item in the GUI
 	 * @return - the size of the GUI
 	 */
 	public int getSize(int lastItem, int size) {
-		if(this == SizeType.CUSTOM) {
+		if (this == SizeType.CUSTOM) {
 			return size;
-		} else if(this == SizeType.LARGEST) {
+		} else if (this == SizeType.LARGEST) {
 			return Gui.MAXSIZE;
 		} else {
 			size = 9;
@@ -33,13 +53,13 @@ SMALLEST/** Used to make the gui as small as possible*/, LARGEST/** makes the gu
 
 				size += 9;
 			}
-		
-			if(this == SizeType.BLANKROW) {
-				size = (size+9 > Gui.MAXSIZE) ? size : size + 9;
+
+			if (this == SizeType.BLANKROW) {
+				size = (size + 9 > Gui.MAXSIZE) ? size : size + 9;
 			}
-		
+
 			return size;
-		
+
 		}
 	}
 
