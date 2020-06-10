286b8fe25e95343710f77f71056c40d3bfb876bf
James McNair <booksaw224@gmail.com>
2020-06-10 12:37:11 +0100

    More improvements to pageLayout

--- src/com/booksaw/guiAPI/preBuilt/PageLayout.java
+++ src/com/booksaw/guiAPI/preBuilt/PageLayout.java
@@ -22,7 +22,7 @@ import com.booksaw.guiAPI.API.items.itemActions.ItemAction;
  * @author booksaw
  *
  */
-public abstract class PageLayout extends Gui implements ItemAction {
+public abstract class PageLayout extends Gui {
 
 	/**
 	 * Used to track what page the player is viewing
@@ -30,7 +30,7 @@ public abstract class PageLayout extends Gui implements ItemAction {
 	private HashMap<Player, Integer> pages;
 
 	List<GuiItem> contents;
-	protected GuiItem[] bottomBar;
+	protected GuiItem[] bottomBar = null;
 
 	@Override
 	protected void layout(ItemCollection items) {
@@ -40,12 +40,12 @@ public abstract class PageLayout extends Gui implements ItemAction {
 		bottomBar = new GuiItem[9];
 
 		GuiItem back = DefaultItem.BACK.getItem();
-		back.addAction(this);
+		back.addAction(new EventHandle());
 		back.setActionCommand("back");
 		bottomBar[3] = back;
 
 		GuiItem foward = DefaultItem.FOWARD.getItem();
-		foward.addAction(this);
+		foward.addAction(new EventHandle());
 		foward.setActionCommand("foward");
 		bottomBar[5] = foward;
 		configureBottomBar(bottomBar);
@@ -56,13 +56,15 @@ public abstract class PageLayout extends Gui implements ItemAction {
 	protected void buildGui(Player p, ItemCollection items) {
 		List<GuiItem> contents = new ArrayList<>(this.contents);
 		addItems(p, contents);
+		GuiItem[] bottomBar = this.bottomBar.clone();
 		configureBottomBar(p, bottomBar);
 
 		size = GuiUseful.getSize(contents.size());
-		if (size < 0 || size > 54) {
+		if (size < 0 || size >= 54) {
 			size = 54;
+		} else {
+			size += 9;
 		}
-
 		int contentSize = size - 9;
 
 		// adding the bottom bar
@@ -137,40 +139,6 @@ public abstract class PageLayout extends Gui implements ItemAction {
 	}
 
 	@Override
-	public void onEvent(GuiEvent e) {
-		int page;
-		Integer pageInt;
-
-		switch (e.getActionCommand()) {
-		case "foward":
-			// getting the page that the player is viewing
-			pageInt = pages.get((Player) e.getPlayer()) + 1;
-			if (pageInt == null || pageInt < 0) {
-				page = 0;
-			} else {
-				page = pageInt;
-			}
-
-			pages.put((Player) e.getPlayer(), page);
-			displayGui((Player) e.getPlayer());
-			break;
-		case "back":
-			// getting the page that the player is viewing
-			pageInt = pages.get((Player) e.getPlayer()) - 1;
-
-			if (pageInt == null || pageInt < 0) {
-				page = 0;
-			} else {
-				page = pageInt;
-			}
-
-			pages.put((Player) e.getPlayer(), page);
-			displayGui((Player) e.getPlayer());
-			break;
-		}
-	}
-
-	@Override
 	public void onClose(Player player) {
 		pages.remove(player);
 	}
@@ -183,5 +151,51 @@ public abstract class PageLayout extends Gui implements ItemAction {
 	@Override
 	public void onDeath(Player player) {
 		pages.remove(player);
+	}
+
+	/**
+	 * This is used to handle the foward and back buttons without pageLayout
+	 * extending Item action
+	 * 
+	 * @author nfgg2
+	 *
+	 */
+	private class EventHandle implements ItemAction {
+
+		@Override
+		public void onEvent(GuiEvent e) {
+			int page;
+			Integer pageInt;
+
+			switch (e.getActionCommand()) {
+			case "foward":
+				// getting the page that the player is viewing
+				pageInt = pages.get((Player) e.getPlayer()) + 1;
+				if (pageInt == null || pageInt < 0) {
+					page = 0;
+				} else {
+					page = pageInt;
+				}
+
+				pages.put((Player) e.getPlayer(), page);
+				displayGui((Player) e.getPlayer());
+				break;
+			case "back":
+				// getting the page that the player is viewing
+				pageInt = pages.get((Player) e.getPlayer()) - 1;
+
+				if (pageInt == null || pageInt < 0) {
+					page = 0;
+				} else {
+					page = pageInt;
+				}
+
+				pages.put((Player) e.getPlayer(), page);
+				displayGui((Player) e.getPlayer());
+				break;
+			}
+
+		}
+
 	}
 }
